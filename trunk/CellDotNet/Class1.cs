7371d1b9f16922fa47bf009d00fd552351c3f241
klausmh <klausmh@b8d2d9dc-6a2a-0410-996f-d3b32c843a95>
2007-07-08 21:59:28 +0000

    Able to run code on SPU :)

--- trunk/CellDotNet/Class1.cs
+++ trunk/CellDotNet/Class1.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Reflection;
+using System.Text;
 using Mono.Cecil;
 using Mono.Cecil.Cil;
 using Mono.Cecil.Metadata;
@@ -17,6 +18,7 @@ namespace CellDotNet
 //			TypeExperimenalStuff(3);
 //			new MethodCompilerTest().TestBuildTree();
 //			DoExtremelySimpleParameterCodeGen();
+			System.Console.WriteLine("Program done.");
 		}
 
 		private static void GenericExperiment()
@@ -35,6 +37,9 @@ namespace CellDotNet
 
 		private static void DoExtremelySimpleParameterCodeGen()
 		{
+
+
+
 			RefArgumentDelegate del = delegate(ref int i) { i = 0x1ffff; };
 			MethodDefinition method = GetMethod(del);
 
@@ -57,29 +62,44 @@ namespace CellDotNet
 
 		private unsafe static void RunSpu()
 		{
+//			SpeContext ctxt = new SpeContext();
+//			ctxt.LoadProgram(new int[] { 13 });
+//	
+//			int[] lsa = ctxt.GetCopyOffLocalStorage();
+//
+//			if(lsa[0] != 13)
+//				Console.WriteLine("øv");
+//			else
+//				Console.WriteLine("fint!!!");
+//		
+//
+//			return;
+
+			
 			BasicTestDelegate del = delegate()
-			                        	{
-			                        		int* i;
-			                        		i = (int*)30000;
-			                        		*i = 34;
-			                        	};
-
-
-/*			BasicTestDelegate del = delegate()
 										{
-											int a;
-											int i = 42;
-											if (i >= 9)
-												a = 1;
-											else
-												a = 2;
+											int* i;
+											i = (int*)0x40;
+											*i = 34;
 										};
-*/
+
+
+			/*			BasicTestDelegate del = delegate()
+													{
+														int a;
+														int i = 42;
+														if (i >= 9)
+															a = 1;
+														else
+															a = 2;
+													};
+			*/
 			MethodDefinition method = GetMethod(del);
 			MethodCompiler ci = new MethodCompiler(method);
 			ci.PerformProcessing(MethodCompileState.TreeConstructionDone);
 
 			new TreeDrawer().DrawMethod(ci, method);
+
 			ILTreeSpuWriter writer = new ILTreeSpuWriter();
 			SpuInstructionWriter ilist = new SpuInstructionWriter();
 			writer.GenerateCode(ci, ilist);
@@ -88,31 +108,199 @@ namespace CellDotNet
 			Console.WriteLine();
 			Console.WriteLine("Disassembly: ");
 			Console.WriteLine(ilist.Disassemble());
-
+			
 
 			RegAlloc regalloc = new RegAlloc();
 			List<SpuInstruction> asm = new List<SpuInstruction>(ilist.Instructions);
 			regalloc.alloc(asm, 16);
+
 			int[] bincode = SpuInstruction.emit(asm);
-			return;
+			int[] testbincode = new int[10];
 
-			
+			testbincode[0] = 0;
+			testbincode[1] = 0;
+			testbincode[2] = 0;
+			testbincode[3] = 0;
+			testbincode[4] = 0;
+			testbincode[5] = 0;
+			testbincode[6] = 0;
+			testbincode[7] = 0;
+			testbincode[8] = 0;
+			testbincode[9] = 0;
+
+//			testbincode[0] = Convert.ToInt32("010000011", 2) << (32 - 9) | (34 << 7) | 8; //ilh i rt
+//			testbincode[1] = Convert.ToInt32("010000010", 2) << (32 - 9) | (0 << 7) | 8; //ilhu i rt
+//			testbincode[2] = Convert.ToInt32("001000001", 2) << (32 - 9) |  (7 << 7) | 8; //stqa i rt, addr = i 
+//			testbincode[3] = 34;
+
+//			testbincode[0] = 34;
+
+//			testbincode[0] = Convert.ToInt32("010000001", 2) << (32 - 9) | (34 << 7) | 8; //il i rt
+//			testbincode[1] = Convert.ToInt32("001000001", 2) << (32 - 9) |  (15 << 7) | 8; //stqa i rt, addr = i 
+//			testbincode[2] = 34;
+
+			testbincode[0] = Convert.ToInt32("010000001", 2) << (32 - 9) | (34 << 7) | 8; //il i rt
+			testbincode[1] = Convert.ToInt32("001100101", 2) << (32 - 9) | (0xf000 << 7) | 9; //fsmbi i rt
+			testbincode[2] = Convert.ToInt32("00011000001", 2) << (32 - 11) | (8 << 14) | (9 << 7) | 10; //and i rt
+			testbincode[3] = Convert.ToInt32("001000001", 2) << (32 - 9) | (8 << 7) | 10; //stqa i rt, addr = i 
+			testbincode[4] = 34;
+
 			SpeContext ctx = new SpeContext();
-			ctx.LoadProgram(bincode);
-//            Buffer.BlockCopy(myspucode, 0, ctx.LocalS myspycode.Length, );
-			// copy code to spu...
-			ctx.Run();
 
-			int* i2;
-			i2 = (int*) ctx.LocalStorage + 30000;
-			if (*i2 != 34)
+//			Console.WriteLine("LocalStorageSize = {0}", ctx.LocalStorageSize);
+
+//			int[] ls1 = ctx.GetCopyOffLocalStorage();
+
+			if (!ctx.LoadProgram(bincode))
+//			if (!ctx.LoadProgram(testbincode))
+			{
+				System.Console.WriteLine("Programe load failed!");
+				return;
+			}
+				
+//			int[] ls2 = ctx.GetCopyOffLocalStorage();
+
+			//            Buffer.BlockCopy(myspucode, 0, ctx.LocalS myspycode.Length, );
+			// copy code to spu...
+			int r = ctx.Run();
+//			Console.WriteLine("ctx.Run(): {0}", r);
+
+//			int[] ls3 = ctx.GetCopyOffLocalStorage();
+
+//			Console.WriteLine("Programe code:");
+//			for (int i = 0; i < bincode.Length; i++ )
+//				Console.Write("{0,8:X} ", bincode[i]);
+//			Console.WriteLine();
+
+
+//			Console.WriteLine("ls1 == ls2");
+//			WriteDiff(ls1, ls2);
+
+//			Console.WriteLine("ls2 == ls3");
+//			WriteDiff(ls2, ls3);
+
+//			Console.WriteLine("Finde value:");
+
+//			FindeValue(ls1, 255);
+//			FindeValue(ls2, 255);
+//			FindeValue(ls3, 255);
+
+//			Console.WriteLine("DiffCount ls1 ls2: {0}", CountDiff(ls1, ls2));
+//			Console.WriteLine("DiffCount ls2 ls3: {0}", CountDiff(ls2, ls3));
+//			Console.WriteLine("DiffCount ls1 ls3: {0}", CountDiff(ls1, ls3));
+
+			int[] ls = ctx.GetCopyOffLocalStorage();
+
+
+//			int* i2;
+//			i2 = (int*)ctx.LocalStorage + 0xff0;
+
+//			for (int i = 0; i < 0x80/4; i++)
+//			{
+//				Console.WriteLine("ls[i]: {0,8:x}", ls[i]);
+//				Console.WriteLine("  {0,2:x} {1,2:x} {2,2:x} {3,2:x}", (ls[i] >> 24) & 0xff, (ls[i] >> 16) & 0xff, (ls[i] >> 8) & 0xff, ls[i] & 0xff);
+//				if (ls[i] == 34)
+//					Console.WriteLine("i: {0}", i);
+//			}
+
+			if (ls[0x40/4] != 34)
+			{
 				Console.WriteLine("øv");
+				Console.WriteLine("Value: {0}", ls[0xff0/4]);
+			}
 			else
 				Console.WriteLine("Selvfølgelig :)");
 
+			return;
+
 		}
 
-		unsafe private static void RefIntMethod(int *i) {}
+		private static int CountDiff(int[] a1, int[] a2)
+		{
+			int count = 0;
+			int minsize = (a1.Length < a2.Length) ? a1.Length : a2.Length;
+			for (int i = 0; i < minsize; i++)
+				if (a1[i] != a2[i])
+					count++;
+			return count;
+		}
+
+		private static void FindeValue(int[] a, int value)
+		{
+			int find = 0;
+
+			for(int i = 0; i<a.Length; i++)
+			{
+				if(a[i] == value)
+				{
+					Console.Write("{0} ", i);
+					find++;
+				}
+			}
+
+			Console.WriteLine("Value founde {0} places.", find);
+			
+		}
+
+		private static void WriteDiff(int[] a1, int[] a2)
+		{
+			System.Text.StringBuilder line1 = null;
+			System.Text.StringBuilder line2 = null;
+			int linewrites = 0;
+			int maxlinewrits = 16;
+
+			int minsize = (a1.Length < a2.Length) ? a1.Length : a2.Length;
+			int maxwrites = maxlinewrits*20;
+			int writs = 0;
+			bool writing = false;
+
+			for(int i = 0; i < minsize && writs <= maxwrites; i++)
+			{
+				if (a1[i] != a2[i])
+				{
+					if (!writing)
+					{
+						Console.WriteLine("Diff index = {0}", i);
+						line1 = new StringBuilder();
+						line2 = new StringBuilder();
+						linewrites = 0;
+						writing = true;
+					} else if (linewrites >= maxlinewrits)
+					{
+						Console.WriteLine(line1.ToString());
+						Console.WriteLine(line2.ToString());
+						Console.WriteLine();
+
+						line1 = new StringBuilder();
+						line2 = new StringBuilder();
+
+						linewrites = 0;
+					}
+
+					line1.AppendFormat("{0,8:X} ", a1[i]);
+					line2.AppendFormat("{0,8:X} ", a2[i]);
+					writs++;
+					linewrites++;
+				}
+				else if(writing)
+				{
+					Console.WriteLine(line1.ToString());
+					Console.WriteLine(line2.ToString());
+					Console.WriteLine();
+
+					line1 = new StringBuilder();
+					line2 = new StringBuilder();
+
+					linewrites = 0;
+
+					writing = false;
+				}
+			}
+
+			Console.WriteLine();
+		}
+
+		unsafe private static void RefIntMethod(int* i) { }
 
 		private static void TypeExperimenalStuff(int i)
 		{
@@ -129,11 +317,11 @@ namespace CellDotNet
 			TypeDescription td = tc.GetTypeDescription(tint);
 			TypeDescription td2 = tc.GetTypeDescription(trefint);
 
-			AssemblyNameReference anref = (AssemblyNameReference) tint.Scope;
+			AssemblyNameReference anref = (AssemblyNameReference)tint.Scope;
 			AssemblyDefinition def = tint.Module.Assembly.Resolver.Resolve(anref);
 
-//			Console.WriteLine("cecil method token: " + method.MetadataToken.ToUInt());
-//			Console.WriteLine("reflection method token: " + del.Method.MetadataToken);
+			//			Console.WriteLine("cecil method token: " + method.MetadataToken.ToUInt());
+			//			Console.WriteLine("reflection method token: " + del.Method.MetadataToken);
 		}
 
 		public static MethodDefinition GetMethod(Delegate a)
@@ -145,7 +333,7 @@ namespace CellDotNet
 		private static MethodDefinition GetMethod(MethodInfo m)
 		{
 			AssemblyDefinition ass = AssemblyFactory.GetAssembly(m.DeclaringType.Assembly.Location);
-			return (MethodDefinition) ass.MainModule.LookupByToken(new MetadataToken(m.MetadataToken));
+			return (MethodDefinition)ass.MainModule.LookupByToken(new MetadataToken(m.MetadataToken));
 		}
 	}
 }
