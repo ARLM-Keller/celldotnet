7513e3f0eb7382a41e93e2c56304cc06ac4bb965
halland <halland@b8d2d9dc-6a2a-0410-996f-d3b32c843a95>
2007-09-30 22:35:40 +0000

    Marshaler tests.

--- trunk/CellDotNet/MarshalerTest.cs
+++ trunk/CellDotNet/MarshalerTest.cs
@@ -18,5 +18,27 @@ namespace CellDotNet
 			object[] arr2 = new Marshaler().GetValues(buf, new Type[] { typeof(int), typeof(float), typeof(double), typeof(short) });
 			AreEqual(arr, arr2);
 		}
+
+		[Test]
+		public void TestVectorTypes()
+		{
+			object[] arr = new object[] { new Int32Vector(1, 2, 3, 4), new Float32Vector(1, 2, 3, 4) };
+			byte[] buf = new Marshaler().GetArguments(arr);
+
+			AreEqual(arr.Length * 16, buf.Length);
+			object[] arr2 = new Marshaler().GetValues(buf, new Type[] { typeof(Int32Vector), typeof(Float32Vector) });
+			AreEqual(arr, arr2);
+		}
+
+		[Test]
+		public void TestOtherStructs()
+		{
+			object[] arr = new object[] { new MainStorageArea((IntPtr) 0x12323525), (IntPtr) 0x34985221 };
+			byte[] buf = new Marshaler().GetArguments(arr);
+
+			AreEqual(arr.Length * 16, buf.Length);
+			object[] arr2 = new Marshaler().GetValues(buf, new Type[] { typeof(MainStorageArea), typeof(IntPtr) });
+			AreEqual(arr, arr2);
+		}
 	}
 }
