37fb7d0cbd8413236f9c908e539c123496b3f95c
klausmh <klausmh@b8d2d9dc-6a2a-0410-996f-d3b32c843a95>
2007-10-17 14:37:19 +0000

    Demo.

--- SciMarkCell/SORSingleCell.cs
+++ SciMarkCell/SORSingleCell.cs
@@ -1,52 +1,27 @@
-/// <license>
-/// This is a port of the SciMark2a Java Benchmark to C# by
-/// Chris Re (cmr28@cornell.edu) and Werner Vogels (vogels@cs.cornell.edu)
-/// 
-/// For details on the original authors see http://math.nist.gov/scimark2
-/// 
-/// This software is likely to burn your processor, bitflip your memory chips
-/// anihilate your screen and corrupt all your disks, so you it at your
-/// own risk.
-/// </license>
-
-using System;
 using CellDotNet;
 
 namespace SciMark2Cell
 {
 	public class SORSingleCell
 	{
-		public static float num_flops(int M, int N, int num_iterations)
+		public static void execute(float omega, MainStorageArea G_msa, int M, int N, int iterations)
 		{
-			float Md = M;
-			float Nd = N;
-			float num_iterD = num_iterations;
+			float[] G = new float[M * N];
 
-			return (Md - 1) * (Nd - 1) * num_iterD * 6.0f;
+			Mfc.Get(G, G_msa);
+
+			execute_inner(omega, G, M, N, iterations);
 		}
 
-		public static void execute(float omega, MainStorageArea G, int M, int N, int num_iterations)
+		public static void execute_inner(float omega, float[] G, int M, int N, int iterations)
 		{
-			float[] Gv = new float[M * N];
-
-			Mfc.Get(Gv, G);
-
-			execute_inner(omega, Gv, M, N, num_iterations);
-		}
-
-		public static void execute_inner(float omega, float[] G, int M, int N, int num_iterations)
-		{
-//			int M = G.Length;
-//			int N = G[0].Length;
-
 			float omega_over_four = omega * 0.25f;
 			float one_minus_omega = 1.0f - omega;
 
 			// update interior points
-			//
 			int Mm1 = M - 1;
 			int Nm1 = N - 1;
-			for (int p = 0; p < num_iterations; p++)
+			for (int p = 0; p < iterations; p++)
 			{
 				for (int i = 1; i < Mm1; i++)
 				{
