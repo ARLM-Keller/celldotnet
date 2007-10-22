d4a4944b6ee5d7bbd3aa9d4a3671fc9e10d056ac
halland <halland@b8d2d9dc-6a2a-0410-996f-d3b32c843a95>
2007-10-22 16:05:42 +0000

    The non-ebb check-in. IRTreeBuilder.cs and maybe ILReader.cs are problematic and are the only ones with real changes.

--- CellDotNet/TreeDrawer.cs
+++ CellDotNet/TreeDrawer.cs
@@ -1,3 +1,26 @@
+// 
+// Copyright (C) 2007 Klaus Hansen and Rasmus Halland
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -42,7 +65,7 @@ namespace CellDotNet
 			else if (isTarget)
 				Console.ForegroundColor = ConsoleColor.Green;
 */
-			Output.Write(inst.Offset.ToString("x4") + " " + inst.Opcode.Name);
+			Output.Write(((uint)inst.Offset & 0xffff).ToString("x4") + " " + inst.Opcode.Name);
 
 			if (inst.Operand != null)
 			{
@@ -217,7 +240,7 @@ namespace CellDotNet
 
 			foreach (IRBasicBlock block in blocks)
 			{
-				Output.WriteLine(" - Basic block no. {0}:", block.BlockNumber);
+				Output.WriteLine(" - Basic block {0}:", block.BlockNumber);
 				DrawTree(block);
 			}
 		}
